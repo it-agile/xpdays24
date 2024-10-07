f98da5152847c22ac524448b84de5fc6be21c5a7
Tim Müller <apple@muellertim.de>
2024-10-07 11:30:49 +0200

    added pom and empty Test

---
+++ src/test/java/xpdays24/SupermarketTest.java
@@ -0,0 +1,14 @@
+package xpdays24;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SupermarketTest {
+
+	@Test
+	void renameMe() {
+		// assertThat().isEqualTo();
+	}
+
+}
