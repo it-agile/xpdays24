47436249f08ccaf56a2e916d606790fddcd5b566
Tim Müller <apple@muellertim.de>
2024-10-09 09:15:13 +0200

    Rename test method for better fit with kata

--- src/test/java/xpdays24/ShoppingBasketTest.java
+++ src/test/java/xpdays24/ShoppingBasketTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
-public class SupermarketTest {
+public class ShoppingBasketTest {
 
 	@Test
 	void renameMe() {
